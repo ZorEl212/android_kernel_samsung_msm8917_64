ab76c83cf55de6e66554e6cd3a90442babad87a9
sadyqowl1560 <sadyqowl1560@gmail.com>
2020-08-25 13:05:54 +0200

    horse 70 : Add horse 70 stuff

---
+++ build_horse70.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SETUP BUILD ENVIROMENT
+#
+export ARCH=arm64
+export PATH=/home/yes/android-toolchain/aarch64-linux-android-4.9-refs_heads_brillo-m7-mr-dev/bin:$PATH
+
+#CLEAN SOURCE
+#
+OUTDIR=out
+rm -rf $OUTDIR
+mkdir $OUTDIR
+make clean  $OUTDIR && make mrproper $OUTDIR
+
+#MAKE DEFCONFIG
+#
+CCV="CROSS_COMPILE=aarch64-linux-android-"
+make -C $(pwd) O=$OUTDIR $CCV j4p_horse70_defconfig
+# make nconfig -C $(pwd) O=$OUTDIR $CCV
+# exit
+
+#GET CPU COUNT AND BUILD
+#
+CORE_COUNT=`grep processor /proc/cpuinfo|wc -l`
+make -j$CORE_COUNT -C $(pwd) O=$OUTDIR $CCV
